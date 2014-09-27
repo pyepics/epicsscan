8757dc98b9bac18a82d368eab07c95e8b849653e
Matt Newville <newville@cars.uchicago.edu>
2014-09-27 17:43:30 -0500

    fixes  to edit scan definitions page

--- lib/gui/edit_scandefs.py
+++ lib/gui/edit_scandefs.py
@@ -8,7 +8,7 @@ import wx.lib.agw.flatnotebook as flat_nb
 import wx.lib.scrolledpanel as scrolled
 import wx.grid as gridlib
 import wx.dataview as dv
-    
+
 from .gui_utils import (GUIColors, set_font_with_children, YesNo, popup,
                         add_button, pack, SimpleText, check, okcancel,
                         add_subtitle, Font, LCEN, CEN, RCEN, FRAMESTYLE)
@@ -69,7 +69,8 @@ class ScanDefPanel(wx.Panel):
 
         dvstyle = dv.DV_VERT_RULES|dv.DV_ROW_LINES|dv.DV_SINGLE
         self.wids = dv.DataViewListCtrl(self, style=dvstyle)
-                
+
+        self.make_titles()
         self.fill_rows()
 
         sizer = wx.BoxSizer(wx.VERTICAL)
@@ -81,34 +82,34 @@ class ScanDefPanel(wx.Panel):
         bsizer.Add(add_button(bpanel, label='Load Scan',   action=self.onLoad))
         bsizer.Add(add_button(bpanel, label='Rename Scan', action=self.onRename))
         bsizer.Add(add_button(bpanel, label='Erase Scan',  action=self.onErase))
+        bsizer.Add(add_button(bpanel, label='Done',        action=self.onDone))
 
         pack(bpanel, bsizer)
         sizer.Add(bpanel, 0, LCEN, 5)
         self.SetAutoLayout(True)
         self.SetSizer(sizer)
         self.Fit()
-    
+
     def make_titles(self):
-        self.wids.DeleteAllItems()
         for icol, dat in enumerate(self.colLabels):
             title, width = dat
-            self.wids.AppendTextColumn(title,   width=width)                
+            self.wids.AppendTextColumn(title,   width=width)
             col = self.wids.Columns[icol]
             col.Sortable = True
             col.Alignment = wx.ALIGN_LEFT
 
     def fill_rows(self):
-        self.make_titles()
+        self.wids.DeleteAllItems()
         for scan in self.scandb.getall('scandefs',
                                        orderby='last_used_time'):
-            if str(scan.type) != str(self.stype.lower()): 
+            if str(scan.type) != str(self.stype.lower()):
                 continue
             sdat  = json.loads(scan.text)
             axis  = sdat['positioners'][0][0]
             npts  = sdat['positioners'][0][4]
             mtime = scan.modify_time.strftime("%Y-%b-%d %H:%M")
             utime = scan.last_used_time.strftime("%Y-%b-%d %H:%M")
-            self.wids.AppendItem((scan.name, axis, npts, mtime, utime))
+            self.wids.AppendItem((scan.name, axis, "%d"%npts, mtime, utime))
 
     def onLoad(self, event=None):
         if self.wids.HasSelection():
@@ -117,45 +118,57 @@ class ScanDefPanel(wx.Panel):
             main = self.parent.parent.load_scan(name)
 
     def onRename(self, event=None):
-        if self.wids.HasSelection():
-            row  = self.wids.GetSelectedRow()
-            name = self.wids.GetStore().GetValueByRow(row, 0)
-            dlg = RenameDialog(self, name)
-            dlg.Raise()
-            if dlg.ShowModal() == wx.ID_OK:
-                newname = dlg.newname.GetValue()
+        if not self.wids.HasSelection():
+            return
+        row  = self.wids.GetSelectedRow()
+        name = self.wids.GetStore().GetValueByRow(row, 0)
+        dlg = RenameDialog(self, name)
+        if dlg.ShowModal() != wx.ID_OK:
+            return
 
-                cls, table = self.scandb.get_table('scandefs')
-                scans = self.scandb.query(table).filter(cls.name==name).all()
-                if scans is not None:
-                    for s in scans:
-                        if s.name == name and s.type == self.stype:
-                            self.scandb.rename_scandef(s.id, newname)
-                    self.scandb.commit()
-                    #wx.CallAfter(self.fill_rows)
+        cls, table = self.scandb.get_table('scandefs')
 
-                store = self.wids.GetStore()
-                # print 'RENAMED  to ', newname
-                # print dir(store)
-                #store.SetValueByRow(row, 0, newname)
+        newname = dlg.newname.GetValue()
+        tmpscans = self.scandb.query(table).filter(cls.name==newname).all()
+        if tmpscans is not None and len(tmpscans) > 0:
+            ret = popup(self, "Scan definition '%s' already in use" % newname,
+                "Scan definition in use!", style=wx.OK)
+            return
+
+        scans = self.scandb.query(table).filter(cls.name==name).all()
+        if scans is not None:
+            scan = scans[0]
+            if scan.name == name and scan.type == self.stype:
+                self.scandb.rename_scandef(scan.id, newname)
+            self.scandb.commit()
+            self.wids.GetStore().SetValueByRow(newname, row, 0)
 
     def onErase(self, event=None):
         if self.wids.HasSelection():
-            row  = self.wids.GetSelectedRow()
-            name = self.wids.GetStore().GetValueByRow(row, 0)
-            stype = self.stype
-            ret = popup(self, "Erase scan definition '%s'?" % name,
-                        "Really Erase Scan definition?",                         
-                        style=wx.YES_NO|wx.NO_DEFAULT|wx.ICON_QUESTION)
-            if ret == wx.ID_YES:
-                cls, table = self.scandb.get_table('scandefs')
-                scans = self.scandb.query(table).filter(cls.name==name).all()
-                if scans is not None:
-                    for s in scans:
-                        if s.name == name and s.type == self.stype:
-                            self.scandb.del_scandef(scanid=s.id)
-                    self.scandb.commit()
-                    #wx.CallAfter(self.fill_rows)
+            return
+
+        row  = self.wids.GetSelectedRow()
+        name = self.wids.GetStore().GetValueByRow(row, 0)
+        stype = self.stype
+        ret = popup(self, "Erase scan definition '%s'?" % name,
+                    "Really Erase Scan definition?",
+                    style=wx.YES_NO|wx.NO_DEFAULT|wx.ICON_QUESTION)
+        if ret != wx.ID_YES:
+            return
+
+        cls, table = self.scandb.get_table('scandefs')
+        scans = self.scandb.query(table).filter(cls.name==name).all()
+        if scans is not None:
+            scan = scans[0]
+            if scan.name == name and scan.type == self.stype:
+                self.scandb.del_scandef(scanid=scan.id)
+            self.scandb.commit()
+            self.wids.GetStore().SetValueByRow(' ', row, 0)
+            wx.CallAfter(self.fill_rows)
+
+    def onDone(self, event=None):
+        self.parent.Destroy()
+
 
 class LinearScanDefs(ScanDefPanel):
     colLabels = (('Scan Name',  175),
@@ -180,10 +193,10 @@ class MeshScanDefs(ScanDefPanel):
         ScanDefPanel.__init__(self, parent, scandb, stype=stype)
 
     def fill_rows(self):
-        self.make_titles()
+        self.wids.DeleteAllItems()
         for scan in self.scandb.getall('scandefs',
                                        orderby='last_used_time'):
-            if str(scan.type) != str(self.stype): 
+            if str(scan.type) != str(self.stype):
                 continue
             sdat  = json.loads(scan.text)
             inner  = sdat['inner'][0]
@@ -191,7 +204,7 @@ class MeshScanDefs(ScanDefPanel):
             npts  = int(sdat['outer'][4]) * int(sdat['inner'][4])
             mtime = scan.modify_time.strftime("%Y-%b-%d %H:%M")
             utime = scan.last_used_time.strftime("%Y-%b-%d %H:%M")
-            self.wids.AppendItem((scan.name, inner, outer, npts, mtime, utime))
+            self.wids.AppendItem((scan.name, inner, outer, "%d"%npts, mtime, utime))
 
 class SlewScanDefs(ScanDefPanel):
     colLabels = (('Scan Name',  175),
@@ -205,10 +218,10 @@ class SlewScanDefs(ScanDefPanel):
         ScanDefPanel.__init__(self, parent, scandb, stype=stype)
 
     def fill_rows(self):
-        self.make_titles()
+        self.wids.DeleteAllItems()
         for scan in self.scandb.getall('scandefs',
                                        orderby='last_used_time'):
-            if str(scan.type) != str(self.stype): 
+            if str(scan.type) != str(self.stype):
                 continue
             sdat  = json.loads(scan.text)
             inner = sdat['inner'][0]
@@ -219,7 +232,7 @@ class SlewScanDefs(ScanDefPanel):
                 npts *= int(sdat['outer'][4])
             mtime = scan.modify_time.strftime("%Y-%b-%d %H:%M")
             utime = scan.last_used_time.strftime("%Y-%b-%d %H:%M")
-            self.wids.AppendItem((scan.name, inner, outer, npts, mtime, utime))
+            self.wids.AppendItem((scan.name, inner, outer, "%d"%npts, mtime, utime))
 
 
 class XAFSScanDefs(ScanDefPanel):
@@ -234,10 +247,10 @@ class XAFSScanDefs(ScanDefPanel):
         ScanDefPanel.__init__(self, parent, scandb, stype=stype)
 
     def fill_rows(self):
-        self.make_titles()
+        self.wids.DeleteAllItems()
         for scan in self.scandb.getall('scandefs',
                                        orderby='last_used_time'):
-            if str(scan.type) != str(self.stype): 
+            if str(scan.type) != str(self.stype):
                 continue
             sdat  = json.loads(scan.text)
             e0   = sdat['e0']
@@ -247,7 +260,7 @@ class XAFSScanDefs(ScanDefPanel):
                 npts += sdat['regions'][ireg][2]
             mtime = scan.modify_time.strftime("%Y-%b-%d %H:%M")
             utime = scan.last_used_time.strftime("%Y-%b-%d %H:%M")
-            self.wids.AppendItem((scan.name, e0, nreg, npts, mtime, utime))
+            self.wids.AppendItem((scan.name, "%.1f"%e0, nreg, "%d"%npts, mtime, utime))
 
 
 class ScandefsFrame(wx.Frame) :
@@ -264,7 +277,7 @@ class ScandefsFrame(wx.Frame) :
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         self.reverse_sort = False
-        
+
         self.SetMinSize((740, 600))
         self.colors = GUIColors()
         panel = scrolled.ScrolledPanel(self)
@@ -272,7 +285,7 @@ class ScandefsFrame(wx.Frame) :
         self.nb = flat_nb.FlatNotebook(panel, wx.ID_ANY, agwStyle=FNB_STYLE)
         self.nb.SetBackgroundColour('#FAFCFA')
         self.SetBackgroundColour('#FAFCFA')
-        
+
         sizer.Add(SimpleText(panel, 'Scan Definitions',
                              font=Font(13),
                              colour=self.colors.title, style=LCEN),
@@ -284,22 +297,22 @@ class ScandefsFrame(wx.Frame) :
                                ('Mesh',   MeshScanDefs),
                                ('Slew',   SlewScanDefs),
                                ('XAFS',   XAFSScanDefs)):
-                               
+
             table = creator(self, self.scandb, stype=pname.lower())
             self.tables[pname.lower()] = table
             self.nb.AddPage(table, "%s Scans" % pname)
             self.nblabels.append((pname.lower(), table))
-            
+
         self.nb.SetSelection(0)
         sizer.Add(self.nb, 1, wx.ALL|wx.EXPAND, 5)
 
-        bpanel = wx.Panel(panel)
-        bsizer = wx.BoxSizer(wx.HORIZONTAL)
-        bsizer.Add(add_button(bpanel, label='Done',   action=self.onDone))
+#         bpanel = wx.Panel(panel)
+#         bsizer = wx.BoxSizer(wx.HORIZONTAL)
+#         bsizer.Add(add_button(bpanel, label='Done',   action=self.onDone))
+#
+#         pack(bpanel, bsizer)
+#         sizer.Add(bpanel, 0, LCEN, 5)
 
-        pack(bpanel, bsizer)
-        sizer.Add(bpanel, 0, LCEN, 5)
-        
         pack(panel, sizer)
         panel.SetupScrolling()
 
@@ -308,7 +321,3 @@ class ScandefsFrame(wx.Frame) :
         pack(self, mainsizer)
         self.Show()
         self.Raise()
-
-    def onDone(self, event=None):
-        self.Destroy()
-            
