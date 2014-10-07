145826aa09d3e8de36e58292d8c8a3e8743b8364
Matt Newville <newville@cars.uchicago.edu>
2014-10-07 17:00:50 -0500

    improvements in server

--- lib/server/scanserver.py
+++ lib/server/scanserver.py
@@ -50,6 +50,12 @@ class ScanServer():
         self.set_scan_message('Server Starting')
         self.larch = larch.Interpreter()
 
+        self.scandb.set_info('request_abort', 0)
+        self.scandb.set_info('request_pause', 0)
+        self.scandb.set_info('request_resume', 0)
+        self.scandb.set_info('request_shutdown', 0)
+
+
         symtab = self.larch.symtable
         symtab.set_symbol(self.LARCH_SCANDB, self.scandb)
         macro_folder = self.scandb.get_info('macros folder')
@@ -157,7 +163,10 @@ class ScanServer():
             command = "do_%s" % command
             args = ', '.join(words)
 
-        larch_cmd = "%s(%s)" % (command, args)
+        if len(args) == 0:
+            larch_cmd = command
+        else:
+            larch_cmd = "%s(%s)" % (command, args)
 
         self.scandb.set_info('current_command', larch_cmd)
         self.larch.error = []
@@ -165,16 +174,17 @@ class ScanServer():
         self.scandb.set_info('scan_status', 'running')
         self.scandb.set_command_status(req.id, 'running')
 
-        out = self.larch.eval(larch_cmd)
+        print 'RUN LARCH COMMAND ', larch_cmd
+        out = self.larch.run(larch_cmd)
         time.sleep(0.1)
         if len(self.larch.error) > 0:
             self.scandb.set_info('command_error', repr(self.larch.error[0].msg))
 
-        self.scandb.set_info('command_running', 0)
-        self.scandb.set_command_output(req.id, repr(out))
+        # print 'Command near done ', req.id, type(req.id)
+        
         self.scandb.set_command_status(req.id, 'finished')
-        self.scandb.set_info('scan_status', 'idle')
-        self.scandb.commit()
+        # self.scandb.set_info('scan_status', 'idle')
+        # self.scandb.commit()
         self.command_in_progress = False
 
     def look_for_interrupt_requests(self):
@@ -198,16 +208,25 @@ class ScanServer():
         msgtime = time.time()
         self.set_scan_message('Server Ready')
         while True:
-            self.sleep(0.25)
-            self.look_for_interrupt_requests()
-            if self.req_shutdown:
+            try:
+                self.sleep(0.25)
+                self.look_for_interrupt_requests()
+                if self.req_shutdown:
+                    print 'req shudown!!'
+                    break
+
+            except KeyboardInterrupt:
                 break
+                
             reqs = self.scandb.get_commands('requested')
-            if (time.time() - msgtime )> 600:
+            if (time.time() - msgtime )> 60:
                 print '#Server Alive, %i Pending requests' % len(reqs)
                 msgtime = time.time()
             elif len(reqs) > 0:
-                self.do_command(reqs.pop(0))
+                for req in self.scandb.get_commands('requested'):
+                    self.do_command(req)
+
+
         # mainloop end
         self.finish()
         sys.exit()
