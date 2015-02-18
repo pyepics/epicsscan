ca07787c5ba51d340234125abbdbf7c4e536e472
Matt Newville <newville@cars.uchicago.edu>
2015-02-17 22:41:45 -0600

    more improvements for showing command sequence

--- lib/gui/edit_sequences.py
+++ lib/gui/edit_sequences.py
@@ -1,20 +1,28 @@
 
 import sys
 import time
-
+from datetime import datetime, timedelta
 import wx
 import wx.lib.scrolledpanel as scrolled
 
 from ..ordereddict import OrderedDict
-from .gui_utils import GUIColors, set_font_with_children, YesNo
-from .gui_utils import add_button, pack, SimpleText
-import  wx.grid as gridlib
+from .gui_utils import (GUIColors, set_font_with_children, YesNo, popup,
+                        add_button, pack, SimpleText, check, okcancel,
+                        add_subtitle, Font, LCEN, CEN, RCEN, FRAMESTYLE)
+
+RCEN |= wx.ALL
+LCEN |= wx.ALL
+CEN  |= wx.ALL
+
+import wx.grid as gridlib
+
+import wx.dataview as dv
 
 LEFT = wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL|wx.ALL
 CEN  = wx.ALIGN_CENTER|wx.ALIGN_CENTER_VERTICAL|wx.ALL
 
 builtin_macros = """ (text = '<builtin>')
-  name              arguments                         
+  name              arguments
   ca_put            'PVName:str, Value:str'
   ca_get            'PVName:str, OutputValue:str'
   do_scan           'ScanName:enum, OutputFile:str, Nrepeat:int'
@@ -23,18 +31,21 @@ builtin_macros = """ (text = '<builtin>')
   scan_at           'ScanName:enum, SampleName:enum'
 """
 
-colLabels = [' Status ', ' Update Time ', ' Action ', ' Output File ', ' Command ', '', '', '']
 ACTIONS = ('Enable', 'Skip')
 
-Buttons = ['Run', 'Pause', 'Cancel All', 'Abort Current Command', 
-           'Insert Commands Here', 'Add Commands to End']
-
-
+def tfmt(dt):
+    return dt.strftime("%d/%b %H:%M:%S")
 
 class SequencesFrame(wx.Frame) :
     """Edit/Manage/Run/View Sequences"""
-    def __init__(self, parent, pos=(-1, -1), _larch=None):
+    colLabels = (('ID',          50),
+                 ('Status',     100),
+                 ('Command',    300),
+                 ('Modifed',    125),
+                 ('Created',    125),
+                 )
 
+    def __init__(self, parent, pos=(-1, -1), _larch=None):
         self.parent = parent
         self.scandb = parent.scandb
 
@@ -46,124 +57,89 @@ class SequencesFrame(wx.Frame) :
         self.Font10=wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD, 0, "")
         titlefont = wx.Font(13, wx.SWISS, wx.NORMAL, wx.BOLD, 0, "")
 
-        wx.Frame.__init__(self, None, -1,
-                          'Epics Scanning: Seuqences')
+        wx.Frame.__init__(self, None, -1, 'Epics Scanning: Command Sequence')
 
         self.SetFont(self.Font10)
-        sizer = wx.GridBagSizer(10, 5)
-        panel = scrolled.ScrolledPanel(self, size=(675, 500))
+
+        self.panel = scrolled.ScrolledPanel(self, size=(700, 325))
         self.colors = GUIColors()
-        panel.SetBackgroundColour(self.colors.bg)
+        self.panel.SetBackgroundColour(self.colors.bg)
 
-        # title row
-        title = SimpleText(panel, 'Extra PVs Setup',  font=titlefont,
-                           colour=self.colors.title, style=tstyle)
+        self.cmdlist = dv.DataViewListCtrl(self.panel,
+                                        style=dv.DV_VERT_RULES|dv.DV_ROW_LINES|dv.DV_SINGLE)
 
-        sizer.Add(title,        (0, 0), (1, 3), LEFT, 5)
+        self.cmdlist.SetMinSize((725, 200))
 
-        ir = 1
-        sizer.Add(SimpleText(panel, label='Description', size=(175, -1)),
-                  (ir, 0), (1, 1), rlabstyle, 2)
-        sizer.Add(SimpleText(panel, label='PV Name', size=(175, -1)),
-                  (ir, 1), (1, 1), labstyle, 2)
-        sizer.Add(SimpleText(panel, label='Use?', size=(60, -1)),
-                  (ir, 2), (1, 1), labstyle, 2)
-        sizer.Add(SimpleText(panel, label='Erase?', size=(60, -1)),
-                  (ir, 3), (1, 1), labstyle, 2)
+        self.panel.SetupScrolling()
 
-        self.widlist = []
-        for this in self.scandb.getall('extrapvs'):
-            desc   = wx.TextCtrl(panel, -1, value=this.name, size=(175, -1))
-            pvctrl = wx.TextCtrl(panel, value=this.pvname,  size=(175, -1))
-            usepv  = YesNo(panel, defaultyes=this.use)
-            delpv  = YesNo(panel, defaultyes=False)
-            
-            ir +=1
-            sizer.Add(desc,   (ir, 0), (1, 1), rlabstyle, 2)
-            sizer.Add(pvctrl, (ir, 1), (1, 1), labstyle, 2)
-            sizer.Add(usepv,  (ir, 2), (1, 1), labstyle, 2)
-            sizer.Add(delpv,  (ir, 3), (1, 1), labstyle, 2)            
-            self.widlist.append((this, desc, pvctrl, usepv, delpv))
+        bpanel = wx.Panel(self)
+        bsizer = wx.BoxSizer(wx.HORIZONTAL)
+        bsizer.Add(add_button(bpanel, label='Cancel Selected',  action=self.onCancel))
+        bsizer.Add(add_button(bpanel, label='Abort Current',    action=self.onAbort))
+        bsizer.Add(add_button(bpanel, label='Abort All',        action=self.onAbortAll))
+        bsizer.Add(add_button(bpanel, label='Done',             action=self.onDone))
+        pack(bpanel, bsizer)
 
-        for i in range(3):
-            desc   = wx.TextCtrl(panel, -1, value='', size=(175, -1))
-            pvctrl = wx.TextCtrl(panel, value='', size=(175, -1))
-            usepv  = YesNo(panel, defaultyes=True)
-            ir +=1
-            sizer.Add(desc,   (ir, 0), (1, 1), rlabstyle, 2)
-            sizer.Add(pvctrl, (ir, 1), (1, 1), labstyle, 2)
-            sizer.Add(usepv,  (ir, 2), (1, 1), labstyle, 2)
-            self.widlist.append((None, desc, pvctrl, usepv, None))
-
-        ir += 1
-        sizer.Add(wx.StaticLine(panel, size=(350, 3), style=wx.LI_HORIZONTAL),
-                  (ir, 0), (1, 4), wx.ALIGN_LEFT|wx.EXPAND, 3)
-        #
-        ir += 1
-        sizer.Add(self.make_buttons(panel), (ir, 0), (1, 2), wx.ALIGN_LEFT, 3)
-
-        pack(panel, sizer)
-
-        panel.SetupScrolling()
-
-        mainsizer = wx.BoxSizer(wx.VERTICAL)
-        mainsizer.Add(panel, 1, wx.GROW|wx.ALL, 1)
-
-        pack(self, mainsizer)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.cmdlist, 1, wx.ALIGN_LEFT)
+        sizer.Add(bpanel, 0, LCEN, 5)
+        self.SetAutoLayout(True)
+        self.SetSizer(sizer)
+        self.Fit()
+        self.make_titles()
+        self.utimer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.fill_rows, self.utimer)
+        self.utimer.Start(10000)
+        self.fill_rows()
         self.Show()
         self.Raise()
 
-    def add_subtitle(self, panel, text):
-        p = wx.Panel(panel)
-        s = wx.BoxSizer(wx.HORIZONTAL)
-        s.Add(wx.StaticLine(p, size=(125, 3), style=wx.LI_HORIZONTAL), 0, LEFT, 5)
-        s.Add(SimpleText(p, text,  colour='#333377'),  0, LEFT, 5)
-        s.Add(wx.StaticLine(p, size=(185, 3), style=wx.LI_HORIZONTAL), 1, LEFT, 5)
-        pack(p, s)
-        return p
+    def make_titles(self):
+        for icol, dat in enumerate(self.colLabels):
+            title, width = dat
+            self.cmdlist.AppendTextColumn(title, width=width)
+            col = self.cmdlist.Columns[icol]
+            col.Sortable = True
+            col.Alignment = wx.ALIGN_LEFT
 
-    def make_buttons(self, panel):
-        btnsizer = wx.StdDialogButtonSizer()
-        btn_ok = wx.Button(panel, wx.ID_OK)
-        btn_no = wx.Button(panel, wx.ID_CANCEL)
-        panel.Bind(wx.EVT_BUTTON, self.onApply, btn_ok)
-        panel.Bind(wx.EVT_BUTTON, self.onClose, btn_no)
-        btn_ok.SetDefault()
-        btnsizer.AddButton(btn_ok)
-        btnsizer.AddButton(btn_no)
+    def fill_rows(self, event=None, **kws):
+        self.cmdlist.DeleteAllItems()
+        self.command_data = {}
+        yesterday = datetime.now() - timedelta(hours=24)
 
-        btnsizer.Realize()
-        return btnsizer
+        for cmd in self.scandb.get_commands(requested_since=yesterday):
+            self.command_data[cmd.id] = cmd
+            cmdid  = "%i" % cmd.id
+            status = self.scandb.status_names[cmd.status_id]
+            rtime  = tfmt(cmd.request_time)
+            mtime  = tfmt(cmd.modify_time)
+            cmdstr = cmd.command
+            if (cmd.arguments not in (None, '') and
+                cmd.output_file not in (None, '')):
+                cmdstr = "%s('%s', '%s')" % (cmdstr, cmd.arguments, cmd.output_file)
+            self.cmdlist.AppendItem((cmdid, status, cmdstr, rtime, mtime))
 
-    def onApply(self, event=None):
-        for w in self.widlist:
-            obj, name, pvname, usepv, erase = w
-            if usepv is not None:
-                usepv = usepv.GetSelection()
-            else:
-                usepv = True
+    def onCancel(self, event=None):
+        print 'onCancel '
+        if self.cmdlist.HasSelection():
+            row  = self.cmdlist.GetSelectedRow()
+            print 'selection ', row
+            print 'cmd id ', self.cmdlist.GetStore().GetValueByRow(row, 0)
 
-            if erase is not None:
-                erase = erase.GetSelection()
-            else:
-                erase = False
-            name   = name.GetValue().strip()
-            pvname = pvname.GetValue().strip()
-            if len(name) < 1 or len(pvname) < 1:
-                continue
-            if erase and obj is not None:
-                self.scandb.del_extrapv(obj.name)
-            elif obj is not None:
-                obj.name = name
-                obj.pvname = pvname
-                obj.use  = usepv
-            elif obj is None:
-                self.scandb.add_extrapv(name, pvname, use=usepv)
+    def onAbort(self, event=None):
+        print 'onAbort '
+        if self.cmdlist.HasSelection():
+            row  = self.cmdlist.GetSelectedRow()
+            print 'selection ', row
+            print 'cmd id ', self.cmdlist.GetStore().GetValueByRow(row, 0)
 
-        self.scandb.commit()
-        self.Destroy()
+    def onAbortAll(self, event=None):
+        print 'onAbort All '
+        if self.cmdlist.HasSelection():
+            row  = self.cmdlist.GetSelectedRow()
+            print 'selection ', row
+            print 'cmd id ', self.cmdlist.GetStore().GetValueByRow(row, 0)
 
 
-    def onClose(self, event=None):
-        self.Destroy()
-
+    def onDone(self, event=None):
+        self.parent.Destroy()
