e9d2f3474fce70edbe19d38ee00b2a87fa328463
Matt Newville <newville@cars.uchicago.edu>
2015-02-17 21:31:03 -0600

    better error handling for larch interface

--- lib/larch_interface.py
+++ lib/larch_interface.py
@@ -5,27 +5,22 @@ import json
 import numpy as np
 import glob
 from .file_utils import nativepath
-from .site_config import get_fileroot, MACRO_FOLDER, LARCH_SCANDB
+from .site_config import get_fileroot, LARCH_SCANDB
 
 import larch
 from larch.utils import OrderedDict
 
 class LarchScanDBServer(object):
-    """
-    """
+    """      """
     def __init__(self, scandb, fileroot=None):
         self.scandb = scandb
         self.fileroot = get_fileroot(fileroot)
         self.larch = larch.Interpreter()
+        self.symtab = self.larch.symtable
+        self.symtab.set_symbol(LARCH_SCANDB, self.scandb)
+        self.symtab._sys.color_exceptions = True
 
-        symtab = self.larch.symtable
-        symtab.set_symbol(LARCH_SCANDB, self.scandb)
-        macro_dir = self.scandb.get_info('macro_folder')
-        if macro_dir is None:
-            macro_dir = MACRO_FOLDER
-            self.scandb.set_info('macro_folder', macro_dir)
-
-        self.macro_dir = macro_dir
+        self.macro_dir = self.scandb.get_info('macro_folder')
         self.loaded_modules = {}
         #self.load_plugins(macro_dir)
         #self.load_modules(macro_dir)
@@ -33,12 +28,22 @@ class LarchScanDBServer(object):
     def load_plugins(self, macro_dir=None):
         if macro_dir is None:
             macro_dir = self.macro_dir
-        plugindir = os.path.join(self.fileroot, macro_dir, 'plugins')
-        self.larch.symtable._sys.config.plugin_paths.insert(0, plugindir)
-        for pyfile in glob.glob(os.path.join(plugindir, '*.py')):
-            plugin_name = str(os.path.split(pyfile)[1][:-3])
-            out = self.larch.run("add_plugin('%s')" % plugin_name)
-            print("add_plugin('%s') --> %s" % (plugin_name, out))
+        if macro_dir is None:
+            print("load_plugins: no Macro folder")
+            return
+        else:
+            plugindir = os.path.join(self.fileroot, macro_dir, 'plugins')
+            self.symtab._sys.config.plugin_paths.insert(0, plugindir)
+            for pyfile in glob.glob(os.path.join(plugindir, '*.py')):
+                plugin_name = str(os.path.split(pyfile)[1][:-3])
+                out = self.larch.run("add_plugin('%s')" % plugin_name)
+                if not out:
+                    print("Error adding plugin '%s'" % (plugin_name))
+                    if len(self.larch.error) > 0:
+                        emsg = '\n'.join(self.larch.error[0].get_error())
+                        self.scandb.set_info('error_message', emsg)
+                else:
+                    print("Added plugin '%s'" % (plugin_name))
 
     def load_modules(self, macro_dir=None, verbose=False):
         """read latest larch modules"""
@@ -47,7 +52,7 @@ class LarchScanDBServer(object):
 
         moduledir = os.path.join(self.fileroot, macro_dir, 'macros')
         origdir = os.getcwd()
-        _sys = self.larch.symtable._sys
+        _sys = self.symtab._sys
         if moduledir not in _sys.path:
             _sys.path.insert(0, moduledir)
         if not os.path.exists(moduledir):
@@ -70,13 +75,14 @@ class LarchScanDBServer(object):
             else:
                 self.larch.run('import %s' % modname)
             if len(self.larch.error) > 0:
-                for err in self.larch.error:
-                    print '==Import Error %s/%s' % (modname, err.get_error())
+                emsg = '\n'.join(self.larch.error[0].get_error())
+                self.scandb.set_info('error_message', emsg)
+                print '==Import Error %s/%s' % (modname, emsg)
             else:
                 if modname not in _sys.searchGroups:
                     _sys.searchGroups.append(modname)
                 self.loaded_modules[modname] = this_mtime
-                thismod  = self.larch.symtable.get_symbol(modname)
+                thismod  = self.symtab.get_symbol(modname)
                 _sys.searchGroupObjects.append(thismod)
 
         # move back to working folder
@@ -84,20 +90,21 @@ class LarchScanDBServer(object):
         return self.get_macros()
 
     def __call__(self, arg):
-        return self.larch.run(arg)
+        return self.run(arg)
 
     def run(self, command):
         self.larch.error = []
         out = self.larch.run(str(command))
         if len(self.larch.error) > 0:
-            print 'Error: ', self.larch.error[0].msg
+            emsg = '\n'.join(self.larch.error[0].get_error())
+            self.scandb.set_info('error_message', emsg)
         return out
 
     def set_symbol(self, name, value):
-        self.larch.symtable.set_symbol(name, value)
+        self.symtab.set_symbol(name, value)
 
     def get_symbol(self, name):
-        return getattr(self.larch.symtable, name)
+        return getattr(self.symtab, name)
 
     def get_error(self):
         return self.larch.error
@@ -111,7 +118,7 @@ class LarchScanDBServer(object):
         returned dictionary has function names as keys, and docstrings as values
         """
         macros = OrderedDict()
-        symtab = self.larch.symtable
+        symtab = self.symtab
         for group in (symtab._epics, symtab._scan, symtab.macros):
             for name in dir(group):
                 obj = getattr(group, name)
