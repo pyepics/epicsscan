9d47da0de5b9620b007459bd8f0e45df5b9ab6e3
Matt Newville <newville@cars.uchicago.edu>
2015-10-27 16:16:51 -0500

    bugfix

--- lib/scandb.py
+++ lib/scandb.py
@@ -1043,16 +1043,13 @@ class InstrumentDB(object):
     def get_position_vals(self, instname, posname):
         """return position with dictionary of PVName:Value pairs"""
         pos = self.get_position(instname, posname)
-
         pv_vals = {}
         _c, ppos_tab = self.scandb.get_table('position_pv')
         _c, pv_tab   = self.scandb.get_table('pvs')
         pvnames = dict([(pv.id, str(pv.name)) for pv in pv_tab.select().execute().fetchall()])
 
         for pvval in ppos_tab.select().where(ppos_tab.c.positions_id == pos.id).execute().fetchall():
-            pvname = pvnames[pvval.pvs_id]
-            if pvname not in exclude_pvs:
-                pv_vals[pvname]= float(pvval.value)
+            pv_vals[ pvnames[pvval.pvs_id]]= float(pvval.value)
         return pv_vals
         
 
