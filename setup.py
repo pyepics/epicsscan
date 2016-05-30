a593dc65abaded3d7fa379c0966f4e9535833c30
Matthew Newville <newville@cars.uchicago.edu>
2016-05-30 09:23:27 -0500

    bump version, add more subdirectories

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup, setup_keywords
 
 deps = ('wx', 'epics', 'numpy', 'matplotlib')
 
-setup(name = 'stepscan',
-      version = '0.2',
+setup(name = 'epicsscan',
+      version = '0.3',
       author = 'Matthew Newville',
       author_email = 'newville@cars.uchicago.edu',
       license = 'BSD',
       description = 'Epics Step Scanning library and applications',
       package_dir = {'epicsscan': 'lib'},
-      packages = ['epicsscan', 'epicsscan.server', 'epicsscan.gui'],
+      packages = ['epicsscan', 'epicsscan.server', 'epicsscan.gui',
+                  'epicsscan.detectors', 'epicsscan.xps'],
       data_files  = [('bin', ['bin/stepscan.py'])])
 
 errmsg = 'WARNING: epics.stepscan requires Python module "%s"'
