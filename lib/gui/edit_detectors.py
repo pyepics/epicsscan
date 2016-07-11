92a951ef43fa80929de2761728e8eaebcd5ad5b0
Matt Newville <newville@cars.uchicago.edu>
2016-07-11 14:22:00 -0500

    add provisional GUI support for TetrAMM

--- lib/gui/edit_detectors.py
+++ lib/gui/edit_detectors.py
@@ -21,8 +21,8 @@ LCEN  |= wx.ALL
 RCEN  |= wx.ALL
 CEN  |= wx.ALL
 
-DET_CHOICES = ('scaler', 'xspress3', 'mca', 'multimca', 'areadetector')
-AD_CHOICES = ['None'] + list(AD_FILE_PLUGINS)
+DET_CHOICES = ('scaler', 'tetramm', 'xspress3', 'mca', 'multimca', 'areadetector')
+AD_CHOICES = ['None'] # + list(AD_FILE_PLUGINS)
 
 class ROIFrame(wx.Frame):
     """Select ROIS"""
