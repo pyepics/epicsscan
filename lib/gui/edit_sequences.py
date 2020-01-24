220f006642d93b6187e8dd2d1e5043ef2dfc1214
Matt Newville <newville@cars.uchicago.edu>
2020-01-24 15:07:18 -0600

    WIP: improving editing of sequence

--- lib/gui/edit_sequences.py
+++ lib/gui/edit_sequences.py
@@ -4,7 +4,8 @@ from datetime import datetime, timedelta
 import wx
 import wx.lib.scrolledpanel as scrolled
 
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
+
 from .gui_utils import (GUIColors, set_font_with_children, YesNo, popup,
                         add_button, pack, SimpleText, check, okcancel,
                         add_subtitle, Font, LCEN, CEN, RCEN, FRAMESTYLE)
@@ -30,23 +31,20 @@ builtin_macros = """ (text = '<builtin>')
   scan_at           'ScanName:enum, SampleName:enum'
 """
 
-ACTIONS = ('Enable', 'Skip')
+CommandRow = namedtuple('CommandRow', ('command', 'status', 'reqtime',
+                                       'updatetime', 'order', 'id'))
+
+DVSTYLE = dv.DV_VERT_RULES|dv.DV_ROW_LINES|dv.DV_MULTIPLE
 
 def tfmt(dt):
-    return dt.strftime("%d/%b %H:%M:%S")
+    return dt.strftime("%b-%d %H:%M")
 
 class SequencesFrame(wx.Frame) :
     """Edit/Manage/Run/View Sequences"""
-    colLabels = (('ID',          50),
-                 ('Status',      75),
-                 ('Command',    375),
-                 ('Requested',  125),
-                 ('Modified',   125),
-                 )
 
-    def __init__(self, parent, pos=(-1, -1), size=(750, 275), _larch=None):
+    def __init__(self, parent, scandb, pos=(-1, -1), size=(800, 400), _larch=None):
         self.parent = parent
-        self.scandb = parent.scandb
+        self.scandb = scandb
 
         style    = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL
         labstyle  = wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL|wx.ALL
@@ -54,19 +52,17 @@ class SequencesFrame(wx.Frame) :
         tstyle    = wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL
 
         self.Font10=wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD, 0, "")
-        titlefont = wx.Font(13, wx.SWISS, wx.NORMAL, wx.BOLD, 0, "")
+        titlefont = wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD, 0, "")
 
         wx.Frame.__init__(self, None, -1,
                           'Epics Scanning: Command Sequence',  size=size)
 
         self.SetFont(self.Font10)
 
-        spanel = scrolled.ScrolledPanel(self, size=(725, 325))
+        spanel = scrolled.ScrolledPanel(self, size=(800, 425))
         self.colors = GUIColors()
         spanel.SetBackgroundColour(self.colors.bg)
-        self.cmdlist = dv.DataViewListCtrl(spanel,
-                                        style=dv.DV_VERT_RULES|dv.DV_ROW_LINES|dv.DV_SINGLE)
-
+        self.cmdlist = dv.DataViewListCtrl(spanel, style=DVSTYLE)
         self.cmdlist.SetMinSize((725, 250))
 
         sizer = wx.BoxSizer(wx.VERTICAL)
@@ -77,8 +73,12 @@ class SequencesFrame(wx.Frame) :
 
         bpan = wx.Panel(self)
         bsiz = wx.BoxSizer(wx.HORIZONTAL)
-        bsiz.Add(add_button(bpan, label='Abort Scan', action=self.onAbort))
-        bsiz.Add(add_button(bpan, label='Cancel All', action=self.onCancelAll))
+        bsiz.Add(add_button(bpan, label='Abort Current Command', action=self.onAbort))
+        bsiz.Add(add_button(bpan, label='Cancel Selected',    action=self.onCancelSelected))
+        bsiz.Add(add_button(bpan, label='Cancel All',         action=self.onCancelAll))
+        bsiz.Add(add_button(bpan, label='Move Selected Up',   action=self.onMoveUp))
+        bsiz.Add(add_button(bpan, label='Move Selected Down', action=self.onMoveDown))
+        bsiz.Add(add_button(bpan, label='Refresh',            action=self.onRefresh))
 
         pack(bpan, bsiz)
 
@@ -90,50 +90,73 @@ class SequencesFrame(wx.Frame) :
         self.Raise()
 
         self.make_titles()
-        self.utimer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.fill_rows, self.utimer)
-        self.utimer.Start(10000)
         self.fill_rows()
         self.Show()
         self.Raise()
 
     def make_titles(self):
-        for icol, dat in enumerate(self.colLabels):
+        cols = (('Command',      420),
+                ('Status',       120),
+                ('Requested',    120),
+                ('Last Updated', 120),
+                ('ID',            80))
+
+        for icol, dat in enumerate(cols):
             title, width = dat
             self.cmdlist.AppendTextColumn(title, width=width)
+
             col = self.cmdlist.Columns[icol]
-            col.Sortable = True
+            col.Sortable = title != 'Command'
             col.Alignment = wx.ALIGN_LEFT
 
     def fill_rows(self, event=None, **kws):
         self.cmdlist.DeleteAllItems()
-        self.command_data = {}
-        yesterday = datetime.now() - timedelta(hours=24)
+        self.commands = {}
+        yesterday = datetime.now() - timedelta(hours=45)
 
         for cmd in self.scandb.get_commands(requested_since=yesterday):
-            self.command_data[cmd.id] = cmd
-            cmdid  = "%i" % cmd.id
+            self.commands[cmd.id] = cmd
             status = self.scandb.status_names[cmd.status_id]
             rtime  = tfmt(cmd.request_time)
             mtime  = tfmt(cmd.modify_time)
             cmdstr = cmd.command
-            if (cmd.arguments not in (None, '') and
-                cmd.output_file not in (None, '')):
-                cmdstr = "%s('%s', '%s')" % (cmdstr, cmd.arguments, cmd.output_file)
-            self.cmdlist.AppendItem((cmdid, status, cmdstr, rtime, mtime))
+            self.cmdlist.AppendItem((cmd.command, status, rtime, mtime,
+                                     repr(cmd.id)))
 
-    def onCancel(self, event=None):
+    def get_row(self, irow):
+        return self.commands[int(self.cmdlist.GetValue(irow, 4))]
+
+    def onMoveUp(self, event=None):
         if self.cmdlist.HasSelection():
-            row  = self.cmdlist.GetSelectedRow()
+            irow = self.cmdlist.GetSelectedRow()
+            row = self.get_row(irow)
+            prow = self.get_row(irow-1) if irow > 0 else None
+            print("Move Up: ", row, prow)
+
+
+    def onMoveDown(self, event=None):
+        if self.cmdlist.HasSelection():
+            irow = self.cmdlist.GetSelectedRow()
+            row = self.get_row(irow)
+            orow = self.get_row(irow+1) if irow < len(self.commands)-1 else None
+            print("Move Down: ", row, orow)
 
     def onAbort(self, event=None):
         self.scandb.set_info('request_abort', 1)
         self.scandb.commit()
         time.sleep(1.0)
 
+
+    def onCancelSelected(self, event=None):
+        if self.cmdlist.HasSelection():
+            row  = self.cmdlist.GetSelectedRow()
+
     def onCancelAll(self, event=None):
         self.scandb.cancel_remaining_commands()
         self.onAbort()
+
+    def onRefresh(self, event=None):
+        self.fill_rows()
 
     def onAbortOLD(self, event=None):
         if self.cmdlist.HasSelection():
