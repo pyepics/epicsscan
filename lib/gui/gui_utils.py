307c4a067a105d4689be7628e74f922f558853e0
Matt Newville <newville@cars.uchicago.edu>
2021-01-19 10:56:03 -0600

    add simplified wrapper for flatnotebook

--- lib/gui/gui_utils.py
+++ lib/gui/gui_utils.py
@@ -26,6 +26,34 @@ LTEXT = wx.ST_NO_AUTORESIZE|wx.ALIGN_CENTER
 FNB_STYLE = flat_nb.FNB_NO_X_BUTTON|flat_nb.FNB_SMART_TABS|flat_nb.FNB_NO_NAV_BUTTONS|flat_nb.FNB_NODRAG
 DVSTYLE = dv.DV_VERT_RULES|dv.DV_ROW_LINES|dv.DV_MULTIPLE
 
+def flatnotebook(parent, paneldict, panelkws={},
+                 on_change=None, drag_tabs=True, selection=0):
+    style = FNB_STYLE
+    if not drag_tabs:
+        style |= flat_nb.FNB_NODRAG
+
+    nb = flat_nb.FlatNotebook(parent, agwStyle=style)
+    nb.SetTabAreaColour(wx.Colour(250, 250, 250))
+    nb.SetActiveTabColour(wx.Colour(254, 254, 195))
+    nb.SetNonActiveTabTextColour(wx.Colour(10, 10, 128))
+    nb.SetActiveTabTextColour(wx.Colour(128, 0, 0))
+    nb.SetPadding(wx.Size(4, -1))
+    nb.DeleteAllPages()
+    nb.pagelist = []
+    grandparent = parent.GetParent()
+    if grandparent is None:
+        grandparent = parent
+    for name, creator in paneldict.items():
+        _page = creator(parent=grandparent, **panelkws)
+        nb.AddPage(_page," %s " % name, True)
+        nb.pagelist.append(_page)
+
+    if callable(on_change):
+        nb.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, on_change)
+
+    nb.SetSelection(selection)
+    return nb
+
 class GUIColors(object):
     """a container for colour attributes
          bg
