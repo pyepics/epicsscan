9cc082d4e8ce488db47a693147fc49c9647d7a1b
Matthew Newville <newville@cars.uchicago.edu>
2024-08-23 16:43:53 -0500

    cleanup exported namespace

--- epicsscan/__init__.py
+++ epicsscan/__init__.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 
-__version__ = '0.6'
-import sys
+__version__ = '0.8'
 
-from .scandb import ScanDB, InstrumentDB
-from .detectors import (get_detector, Trigger, Counter, MotorCounter,
-                        ROISumCounter, SimpleDetector, ScalerDetector,
-                        McaDetector, MultiMcaDetector, AreaDetector)
-from .positioner import Positioner
-from .datafile import ASCIIScanFile
-from .scan import StepScan
-from .xafs_scan import XAFS_Scan, etok, ktoe
+from .simpledb import get_credentials, SimpleDB
 from .scandb_schema import create_scandb
-
-from .create_scan import create_scan
-from .server import run_scanfile, run_scan, debug_scan, read_scanconf, ScanServer
-from .station_config import StationConfig
-from .spec_emulator import SpecScan
+from .scandb import ScanDB, InstrumentDB
+from .server import ScanServer
