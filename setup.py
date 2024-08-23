df2516a5694a3284cc15521c8e8bc8804f9d7dd7
Matthew Newville <newville@cars.uchicago.edu>
2024-08-23 10:13:13 -0500

    use pyproject.toml, stub setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
 #!/usr/bin/env python
-
 from setuptools import setup
-
-deps = ('wx', 'epics', 'numpy', 'matplotlib', 'psycopg2')
-
-setup(name = 'epicsscan',
-      version = '0.5',
-      author = 'Matthew Newville',
-      author_email = 'newville@cars.uchicago.edu',
-      license = 'BSD',
-      description = 'Epics Scanning library and applications',
-      package_dir = {'epicsscan': 'lib'},
-      packages = ['epicsscan', 'epicsscan.server', 'epicsscan.gui',
-                  'epicsscan.detectors'],
-      )
-
-errmsg = "WARNING: epicsscan requires Python module: '%s'"
-for mod in deps:
-    try:
-        a = __import__(mod)
-    except ImportError:
-        print( errmsg % mod)
+if __name__ == "__main__":
+    setup()
